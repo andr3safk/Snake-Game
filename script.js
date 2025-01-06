3044d15fb01755f961ff62a6715f0ed9607e6894
Andres <carrilloandres128@gmail.com>
2025-01-05 19:49:01 -0500

    se crean funciones fundamentales para el juego

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // Se llamana elementos del HTML
 const board = document.getElementById('board');
 const scoreBoard = document.getElementById('scoreBoard');
-const start = document.getElementById('start');
+const startButton = document.getElementById('start');
 const gameOver = document.getElementById('gameOver')
 
 
 // Configuraciones del juego
 const boardSize = 10;
 const gameSpeed = 100;
+
 const squareTypes = {
-  emptySuare: 0,
+  emptySquares: 0,
   snakeSquare: 1,
   foodSquare: 2,
 };
@@ -26,5 +27,34 @@ let snake;
 let score;
 let direction;
 let boardSquares;
-let emptySuares;
+let emptySquares;
 let moveInterval;
+
+
+const createBoard = () => {
+  boardSquares.forEach( (row, rowIndex) => {
+    row.forEach( (column, columnIndex) => {
+      const squareValue = `${rowIndex}${columnIndex}`;
+       const squareElements = document.createElement('div');
+    });
+  });
+};
+
+const setGame = () => {
+  snake = ['00', '01', '02', '03'];
+  score = snake.length;
+  direction = 'ArrowRight';
+  boardSquares = Array.from(Array(boardSize), () => new Array(boardSize).fill(squareTypes.emptySquares));
+  console.log(boardSquares);
+  board.innerHTML= '';
+  emptySquares = [];
+  createBoard();
+};
+
+
+const startGame = () => {
+  setGame();
+
+};
+
+startButton.addEventListener('click', startGame);
