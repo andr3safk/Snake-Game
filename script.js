650a8abfb96699a8e3d3bf9d17f9576fcc3a7e64
Andres <carrilloandres128@gmail.com>
2025-01-05 19:02:19 -0500

    Se crea la estructura basica para el juego

---
+++ script.js
@@ -0,0 +1,4 @@
+const board = document.getElementById('board');
+const scoreBoard = document.getElementById('scoreBoard');
+const start = document.getElementById('start');
+const gameOver = document.getElementById('gameOver')
